2dc65b5e343ff7376671eeac2acd66fa4b99ce72
Glitch (radial-fluorescent-pajama) <none>
2022-08-26 01:36:44 +0000

    🚃🍃 Checkpoint ./index.html:33923274/205 ./js/game_manager.js:33923274/12 ./js/html_actuator.js:33923274/20

--- js/game_manager.js
+++ js/game_manager.js
@@ -78,7 +78,7 @@ GameManager.prototype.addRandomTile = function () {
 // Sends the updated grid to the actuator
 GameManager.prototype.actuate = function () {
   if (this.storageManager.getSatoshisScore() < this.score) {
-    this.storageManager.setSatoshisScore(this.score * 0.005);
+    this.storageManager.setSatoshisScore(this.score / 200);
   }
 
   // Clear the state when the game is over (game over only, not win)
