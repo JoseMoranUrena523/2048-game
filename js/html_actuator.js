1d94d8e394a899f270dde024971722fca44aac58
Jose Moran Urena <jose.moranurena.official@gmail.com>
2022-10-10 15:05:48 -0400

    Update html_actuator.js

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -165,7 +165,8 @@ HTMLActuator.prototype.sendData = function () {
     
     const content = await res.json();
     
-    if (content.data.id === ["74f66389-0746-4156-b944-9b4e00a3b642", "f576c57f-afce-4b9a-8fee-774dcdfd1ef3"]) {
+    if (content.data.id === "74f66389-0746-4156-b944-9b4e00a3b642" ||
+        content.data.id === "f576c57f-afce-4b9a-8fee-774dcdfd1ef3") {
       alert('We have banned you from playing the game due to you either stealing sats, or something else. If you think this is a false ban, please use the sidebar and click "Contact Us" to appeal.');
       console.log(res);
       localStorage.clear();
