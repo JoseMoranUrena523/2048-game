7380192f78b03611a19176c76d92a71b7feab783
Jose Moran Urena <jose@josemoranurena.tech>
2023-02-01 13:43:14 -0500

    Update local_storage_manager.js

--- js/local_storage_manager.js
+++ js/local_storage_manager.js
@@ -1,36 +1,3 @@
-window.fakeStorage = {
-  _data: {},
-
-  setItem: function (id, val) {
-    var secretKey = CryptoJS.lib.WordArray.random(128/8);
-    var hmac = CryptoJS.HmacSHA256(val, secretKey);
-    this._data[id] = {
-        secretKey: secretKey,
-        value: String(val),
-        hmac: hmac
-    };
-  },
-
-  getItem: function (id) {
-    if (this._data.hasOwnProperty(id)) {
-        var data = this._data[id];
-        var hmac = CryptoJS.HmacSHA256(data.value, data.secretKey);
-        if (hmac.toString() === data.hmac.toString()) {
-            return data.value;
-        }
-    }
-    return undefined;
-  },
-
-  removeItem: function (id) {
-    return delete this._data[id];
-  },
-
-  clear: function () {
-    return this._data = {};
-  }
-};
-
 function LocalStorageManager() {
   this.satoshisScoreKey = "satoshisScore";
   this.gameStateKey = "gameState";
@@ -42,7 +9,6 @@ function LocalStorageManager() {
 
 LocalStorageManager.prototype.localStorageSupported = function () {
   var testKey = "test";
-
   try {
     var storage = window.localStorage;
     storage.setItem(testKey, "1");
@@ -53,58 +19,57 @@ LocalStorageManager.prototype.localStorageSupported = function () {
   }
 };
 
-// Best score getters/setters
 LocalStorageManager.prototype.getSatoshisScore = function () {
-  return this.storage.getItem(this.satoshisScoreKey) || 0;
+  var data = this.storage.getItem(this.satoshisScoreKey);
+  if (!data) {
+    return 0;
+  }
+  var hmac = CryptoJS.HmacSHA256(data.value, this.secretKey);
+  if (hmac.toString() === data.hmac.toString()) {
+    return data.value;
+  }
+  return 0;
 };
 
 LocalStorageManager.prototype.setSatoshisScore = function (score) {
-  this.storage.setItem(this.satoshisScoreKey, score);
+  var hmac = CryptoJS.HmacSHA256(score, this.secretKey);
+  this.storage.setItem(this.satoshisScoreKey, {
+    value: score,
+    hmac: hmac
+  });
 };
 
-// Game state getters/setters and clearing
 LocalStorageManager.prototype.getGameState = function () {
-  var state = this.storage.getItem(this.gameStateKey);
-  return state ? JSON.parse(state) : null;
+  var data = this.storage.getItem(this.gameStateKey);
+  if (!data) {
+    return null;
+  }
+  var hmac = CryptoJS.HmacSHA256(data.value, this.secretKey);
+  if (hmac.toString() === data.hmac.toString()) {
+    return JSON.parse(data.value);
+  }
+  return null;
 };
 
 LocalStorageManager.prototype.setGameState = function (gameState) {
-  this.storage.setItem(this.gameStateKey, JSON.stringify(gameState));
+  var hmac = CryptoJS.HmacSHA256(JSON.stringify(gameState), this.secretKey);
+  this.storage.setItem(this.gameStateKey, {
+    value: JSON.stringify(gameState),
+    hmac: hmac
+  });
 };
 
 LocalStorageManager.prototype.clearGameState = function () {
   this.storage.removeItem(this.gameStateKey);
 };
 
-LocalStorageManager.prototype.setItem = function (id, val) {
-  var hmac = CryptoJS.HmacSHA256(val, this.secretKey).toString();
-  this.storage.setItem(id, val + hmac);
-};
-
-LocalStorageManager.prototype.getItem = function (id) {
-  var storedValue = this.storage.getItem(id);
-   if (!storedValue) {
-     return undefined;
-   }
-
-   var data = storedValue.substr(0, storedValue.length - 64);
-   var storedHmac = storedValue.substr(storedValue.length - 64);
-   var calculatedHmac = CryptoJS.HmacSHA256(data, this.secretKey).toString();
-
-   if (calculatedHmac === storedHmac) {
-     return data;
-   }
-};
-
 LocalStorageManager.prototype.verifyHmac = function (id, hmac) {
    var storedValue = this.storage.getItem(id);
    if (!storedValue) {
-     return false;
+      return false;
    }
-
    var data = storedValue.substr(0, storedValue.length - 64);
    var storedHmac = storedValue.substr(storedValue.length - 64);
    var calculatedHmac = CryptoJS.HmacSHA256(data, this.secretKey).toString();
-
    return calculatedHmac === storedHmac;
 };
