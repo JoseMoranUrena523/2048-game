cd73b3fde632bb3275515fe62fbcfacbaa539afc
Jose Moran Urena <jose@josemoranurena.tech>
2023-02-01 13:24:29 -0500

    Update local_storage_manager.js

--- js/local_storage_manager.js
+++ js/local_storage_manager.js
@@ -34,6 +34,7 @@ window.fakeStorage = {
 function LocalStorageManager() {
   this.satoshisScoreKey = "satoshisScore";
   this.gameStateKey = "gameState";
+  this.secretKey = CryptoJS.lib.WordArray.random(128/8);
 
   var supported = this.localStorageSupported();
   this.storage = supported ? window.localStorage : window.fakeStorage;
@@ -75,13 +76,35 @@ LocalStorageManager.prototype.clearGameState = function () {
   this.storage.removeItem(this.gameStateKey);
 };
 
-LocalStorageManager.prototype.verifyHmac = function (id, hmac) {
-   var storedValue = this.storage.getItem(id);
+LocalStorageManager.prototype.setItem = function (id, val) {
+  var hmac = CryptoJS.HmacSHA256(val, this.secretKey).toString();
+  this.storage.setItem(id, val + hmac);
+};
+
+LocalStorageManager.prototype.getItem = function (id) {
+  var storedValue = this.storage.getItem(id);
    if (!storedValue) {
-      return false;
+     return undefined;
    }
+
    var data = storedValue.substr(0, storedValue.length - 64);
    var storedHmac = storedValue.substr(storedValue.length - 64);
    var calculatedHmac = CryptoJS.HmacSHA256(data, this.secretKey).toString();
+
+   if (calculatedHmac === storedHmac) {
+     return data;
+   }
+};
+
+LocalStorageManager.prototype.verifyHmac = function (id, hmac) {
+   var storedValue = this.storage.getItem(id);
+   if (!storedValue) {
+     return false;
+   }
+
+   var data = storedValue.substr(0, storedValue.length - 64);
+   var storedHmac = storedValue.substr(storedValue.length - 64);
+   var calculatedHmac = CryptoJS.HmacSHA256(data, this.secretKey).toString();
+
    return calculatedHmac === storedHmac;
 };
